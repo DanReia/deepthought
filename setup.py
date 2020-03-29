c700bcebb484731ecb7a5166deeb969bc3a080a8
Daniel Correia <dan.reia@gmail.com>
2020-03-29 16:04:08 +0200

    Initial package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='deepthought',
+      version='0.0.1',
+      description='deepthought',
+      author='Daniel Correia',
+      author_email='dan.reia@gmail.com',
+      packages=['deepthought'],
+      python_requires=">=3.7",
+      )
