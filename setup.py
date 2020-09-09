54db7b34135b3812e64933f3e0e0d9d576b94852
Daniel <dan.reia@gmail.com>
2020-09-09 20:26:07 +0200

    Added setuptools_scm

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
-from distutils.core import setup
 
-setup(name='deepthought',
-      version='0.0.1',
-      description='deepthought',
-      author='Daniel Correia',
-      author_email='dan.reia@gmail.com',
-      packages=['deepthought'],
-      python_requires=">=3.7",
-      )
+from setuptools import setup
+
+
+setup(use_scm_version=True)
